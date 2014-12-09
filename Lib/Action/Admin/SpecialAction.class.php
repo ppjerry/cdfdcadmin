2c95e60be28351e19e9cca5f3c07d60b67b4482f
zhibiao <276595510@qq.com>
2014-12-09 14:18:28 +0800

    后台添加特色标签

---
+++ Lib/Action/Admin/SpecialAction.class.php
@@ -0,0 +1,93 @@
+<?php
+/**
+*  特色标签
+*/
+class SpecialAction extends CommonAction {
+  protected $db;
+  function __construct() {
+    parent::__construct();
+    $this->db = D('Special');
+  }
+
+  public function index() {
+    if (isset( $_GET['type'] )) {
+      $specials = $this->db->specialList( array( 'belong' => array( 'in' , array( 0, intval($_GET['type']) ) ) ) );
+    } else {
+      $specials = $this->db->specialList( array( 'belong' => 0 ) );
+    }
+    $this->assign('specials',$specials);
+    $this->assign('types', array( '1' => '新房', '2' => '二手房', '3' => '商铺', '4' => '写字楼', '5' => '别墅' ));
+    $this->display();
+  }
+
+  public function add() {
+    if (IS_POST) {
+      $this->checkToken();
+      $data = $_POST['info'];
+      $data['siteid'] = $this->siteid;
+      if ($id = $this->db->add($data)) {
+        $this->success('添加成功', 'index');
+      } else {
+        $this->success('操作失败', 'index');
+      }
+    } else {
+      $pid = isset( $_GET['pid'] ) ? intval($_GET['pid']) : 0;
+      if ( !empty($pid) ) {
+        $parent_special = $this->db->find($pid);
+        $this->assign( 'parent_special', $parent_special );
+      }
+      $this->assign( 'type', isset($parent_special['belong']) ? $parent_special['belong'] : 0 );
+      $this->assign('types', array( '1' => '新房', '2' => '二手房', '3' => '商铺', '4' => '写字楼', '5' => '别墅' ));
+      $specials = $this->db->where( array( 'pid' => 0 ) )->select();
+      $this->assign( 'pid', $pid );
+      $this->assign( 'specials', $specials );
+      $this->display();
+    }
+  }
+
+  public function edit() {
+    if (IS_POST) {
+      $this->checkToken();
+      $data = $_POST['info'];
+      if ($this->db->where("id = %d",$_POST['id'])->save($data) !== false) {
+        $this->success("更新成功！");
+      } else {
+        $this->error("更新失败! ");
+      }
+    } else {
+      $special = $this->db->find($_GET['id']);
+      $specials = $this->db->where( array( 'pid' => 0 ) )->select();
+      $this->assign('specials', $specials);
+      $this->assign('special', $special);
+      $this->assign('types', array( '1' => '新房', '2' => '二手房', '3' => '商铺', '4' => '写字楼', '5' => '别墅' ));
+      $this->display();
+    }
+  }
+
+  public function listorder() {
+    if (isset($_POST['listorders']) && is_array($_POST['listorders'])) {
+      $sort = $_POST['listorders'];
+      foreach ($sort as $k => $v) {
+        $this->db->where(array('id'=>$k))->save(array('sort'=>$v));
+      }
+    }
+    $this->success('排序成功');
+  }
+
+  public function delete() {
+    if (isset($_POST['ids']) && is_array($_POST['ids'])) {
+      if ($this->db->where(array('id' => array('in', $_POST['ids'])))->delete() !== false) {
+        $this->success('删除成功！');
+      } else {
+        $this->error('删除失败！');
+      }
+    } else {
+      if ($this->db->where(array('id' => $_GET['id']))->delete() !== false) {
+        $this->success('删除成功');
+      } else {
+        $this->error('删除失败');
+      }
+    }
+  }
+}
+?>
---
+++ Lib/Model/SpecialModel.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*  二手房数据库特色标签model
+*/
+class SpecialModel extends UserCenterCommonModel {
+  protected $tableName = 'tag';
+
+  public function specialList( $where = array() ) {
+    load("extend");
+    $specials = $this->order("sort desc");
+    if (!empty($where)) {
+      $specials = $specials->where($where);
+    }
+    $specials = $specials->select();
+    $list = list_to_tree($specials,'id','pid');
+    $specials = array();
+    tree_to_array($list,$specials);
+    return $specials;
+  }
+
+}
